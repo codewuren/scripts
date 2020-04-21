97fd3a623c0cc1308439a5e9deac3e74b255a3b5
codewuren <codewuren@gmail.com>
2020-04-21 08:39:18 +0800

    a

--- autostart.sh
+++ autostart.sh
@@ -5,4 +5,4 @@ fcitx &
 feh --bg-scale ~/Pictures/5.jpg &
 /bin/bash ~/Scripts/dwm-status.sh &
 xset r rate 150 50 &
-xset m 1 0 &
+numlockx &
---
+++ set-mouse-speed.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xinput --set-prop "pointer:Logitech G102 Prodigy Gaming Mouse" "libinput Accel Speed" -0.5
