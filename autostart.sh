1da1d816bc83de6456ebe6a2c3ce22f401aa0c6f
codewuren <codewuren@gmail.com>
2020-05-21 20:27:24 +0800

    a

--- autostart.sh
+++ autostart.sh
@@ -3,7 +3,7 @@
 picom &
 fcitx &
 feh --bg-scale ~/Pictures/1.jpg &
-/bin/bash ~/Scripts/set-mouse-speed.sh &
+/bin/bash ~/code/Scripts/set-mouse-speed.sh &
 xset r rate 150 50 &
 numlockx &
 imwheel &
