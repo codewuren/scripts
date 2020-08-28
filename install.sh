a54a20b5056bd99579639ab42a5f9a32e84379c6
ethanpluviophile <ethanpluviophile@gmail.com>
2020-08-28 14:54:37 +0800

    update

--- install.sh
+++ install.sh
@@ -16,13 +16,16 @@ pacman -S yay git neovim wpa_supplicant dhcpcd iw dialog net-tools networkmanage
 yay -S ttf-linux-libertine ttf-inconsolata ttf-joypixels ttf-twemoji-color noto-fonts-emoji ttf-liberation ttf-droid wqy-bitmapfont wqy-microhei wqy-microhei-lite wqy-zenhei adobe-source-han-mono-cn-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts adobe-source-code-pro-fonts nerd-fonts-source-code-pro noto-fonts ttf-jetbrains-mono
 
 #config /etc/sudoers
-mv ~/GitHub/Scripts/sudoers
+mv ~/GitHub/Scripts/sudoers /etc/sudoers
 
 #add a new user
 useradd -m -G ethan
 
 #changer user's password
 passwd ethan
+
+#move /root/GitHub to /home/ethan/GitHub
+mv /root/GitHub /home/ethan/GitHub
 
 #change user to ethan
 su ethan
@@ -44,6 +47,29 @@ git clone https://github.com/EthanPluviophile/dwm ~/GitHub/dwm
 git clone https://github.com/EthanPluviophile/st ~/GitHub/st
 git clone https://github.com/EthanPluviophile/dmenu ~/GitHub/dmenu
 
+#clone my nvim, neofetch config
+git clone https://github.com/EthanPluviophile/nvim ~/.config/nvim
+git clone https://github.com/EthanPluviophile/neofetch ~/.config.neofetch
+
+#install bash-language-server
+sudo pacman -S bash-language-server
+
+#install ccls
+yay -S ccls
+
+#install python2 and python3
+yay -S python2 python3
+
+#install pip2 amd pip3
+yay -S python2-pip python-pip
+
+#install pynvim
+pip3 install pynvim
+pip2 install pynvim
+
+#use npm to install nvim
+npm install -g nvim
+
 #install dwm
 cd ~/GitHub/dwm
 sudo make clean install
@@ -60,13 +86,13 @@ sudo make clean install
 yay -S google-chrome obs-studio feh gimp cmus pulseaudio pavucontrol lxappearance debtap nvidia nvidia-settings nvidia-utils ueberzug ranger
 
 #config ~/.zshrc
-mv ~/GitHub/Scripts/.zshrc ~/.zshrc
+sudo mv ~/GitHub/Scripts/.zshrc ~/.zshrc
 
 #config /etc/pulse/default.pa
-sudo sh ~/GitHub/Scripts/pulse.sh
+sudo mv ~/GitHub/Scripts/default.pa /etc/pulse/default.pa
 
 #enable NetworkManager
 systemctl enable NetworkManager
 
 #config /etc/X11/xinit/xinitrc
-mv /etc/X11/xinit/xinitrc
+sudo mv ~/GitHub/Scripts/xinitrc /etc/X11/xinit/xinitrc
--- pulse.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo" > /etc/pulse/default.pa
-echo "set-card-profile alsa_card.pci-0000_01_00.1 off" > /etc/pulse/default.pa
