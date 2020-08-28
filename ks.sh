3b9ed0a6b52e50f988a488108425bbf76cb11756
ethanpluviophile <ethanpluviophile@gmail.com>
2020-08-28 10:35:52 +0800

    update

--- ks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-result=$(ps ax|grep -v grep|grep screenkey)
-if [ "$result" == "" ]; then
-  eval "screenkey --bg-color black --font-color white --font JetBrainsMono --opacity 0.4 -t 2 &"
-else
-  eval "killall screenkey"
-fi
--- pulse.sh
+++ pulse.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo "set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo" > /etc/pulse/default.pa
+echo "set-card-profile alsa_card.pci-0000_01_00.1 off" > /etc/pulse/default.pa
