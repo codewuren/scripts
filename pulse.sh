b864eb527d2dbbb472ff8b3b9a959c00e3c3f3fa
ethanpluviophile <ethanpluviophile@gmail.com>
2020-08-29 22:26:22 +0800

    update

---
+++ pulse.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+while true; do:
+	pactl set-card-profile alsa_card.pci-0000_01_00.1 off
+	pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
+	sleep 10
+done
