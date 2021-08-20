111ea2cfa5268f240d66b5cd4597ec529871040b
CodeWuRen <codewuren@gmail.com>
2021-08-20 06:38:55 +0000

    update pulse.sh.

--- pulse.sh
+++ pulse.sh
@@ -3,5 +3,5 @@
 while true; do
 	pactl set-card-profile alsa_card.pci-0000_01_00.1 off
 	pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
-	sleep 10
+	sleep 1
 done
