5a53c3e45e29bc03d2e72ee397bb7fbda9eb1654
CodeWuRen <codewuren@gmail.com>
2021-08-20 07:01:46 +0000

    update autostart.sh.

--- autostart.sh
+++ autostart.sh
@@ -3,9 +3,8 @@
 picom &
 fcitx5 &
 feh --bg-scale ~/Pictures/1.jpg &
-/bin/bash ~/GitHub/Scripts/set-mouse-speed.sh &
-/bin/bash ~/GitHub/Scripts/dwm-status-refresh.sh &
-/bin/bash ~/GitHub/Scripts/pulse.sh &
-nm-applet &
+/bin/bash ~/scripts/set-mouse-speed.sh &
+/bin/bash ~/scripts/dwm-status-refresh.sh &
+/bin/bash ~/scripts/pulse.sh &
 xset r rate 150 50 &
 numlockx &
