a91802b3d7c2a51cbeae03e715da0cdccc8f8378
CodeWuRen <codewuren@gmail.com>
2021-08-20 06:39:13 +0000

    update set-mouse-speed.sh.

--- set-mouse-speed.sh
+++ set-mouse-speed.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-xinput --set-prop "pointer:Logitech G102 Prodigy Gaming Mouse" "libinput Accel Speed" -0.5
+xinput --set-prop "pointer:Logitech G102 Prodigy Gaming Mouse" "libinput Accel Speed" -0.4
