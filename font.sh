4fd5a92ca55cf896d67e36cbc89405b796df3a3b
codewuren <codewuren@gmail.com>
2021-08-25 23:32:44 +0800

    Update

---
+++ font.sh
@@ -0,0 +1,3 @@
+yay -S ttf-linux-libertine ttf-inconsolata ttf-joypixels ttf-twemoji-color noto-fonts-emoji ttf-liberation ttf-droid wqy-bitmapfont wqy-microhei wqy-microhei-lite wqy-zenhei adobe-source-han-mono-cn-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts adobe-source-code-pro-fonts nerd-fonts-source-code-pro noto-fonts ttf-jetbrains-mono
+
+
---
+++ i.sh
@@ -0,0 +1,3 @@
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+
+
