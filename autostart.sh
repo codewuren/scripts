e423af688601a675f07e192ccebba3ae22ff2654
codewuren <codewuren@gmail.com>
2021-12-01 14:39:44 +0800

    update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,17 @@
 #/bin/bash
 
-sleep 1
-xinput --set-prop "pointer:Logitech G304" "libinput Accel Speed" -0.4
-pactl set-card-profile alsa_card.pci-0000_01_00.1 off
-pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
-xset r rate 150 35 &
+COUNT=$(ps -ef |grep bspwm |grep -v "grep" |wc -l)
+
+if [ $COUNT -eq 0 ]; then 
+	echo "bspwm is not running!"
+	exit
+else 
+	while :
+	do
+		xinput --set-prop "pointer:Logitech G304" "libinput Accel Speed" -0.4
+		pactl set-card-profile alsa_card.pci-0000_01_00.1 off
+		pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
+		xset r rate 150 35 &
+		sleep 1
+	done
+fi
