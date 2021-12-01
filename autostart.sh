f83e298bc6362101f780ca964687f19f578f5384
codewuren <codewuren@gmail.com>
2021-12-01 15:35:24 +0800

    update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -2,11 +2,12 @@
 
 COUNT=$(ps -ef |grep bspwm |grep -v "grep" |wc -l)
 
+xinput --set-prop "pointer:Logitech G304" "libinput Accel Speed" -0.4
+xset r rate 150 35
+
 while [ $COUNT -ne 0 ]
 do
-	xinput --set-prop "pointer:Logitech G304" "libinput Accel Speed" -0.4
 	pactl set-card-profile alsa_card.pci-0000_01_00.1 off
 	pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
-	xset r rate 150 35 &
 	sleep 1
 done
