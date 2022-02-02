963bbdd981c3596de58ad19625b49562c4ef4960
codewuren <codewuren@gmail.com>
2022-02-02 09:50:49 +0800

    close screensaver & change dpi

--- autostart.sh
+++ autostart.sh
@@ -3,11 +3,14 @@
 COUNT=$(ps -ef |grep bspwm |grep -v "grep" |wc -l)
 
 xinput --set-prop "pointer:Logitech G304" "libinput Accel Speed" -0
+xrandr --dpi 150
+xset s off
+xset dpms 0 0 0
 xset r rate 150 35
 
 while [ $COUNT -ne 0 ]
 do
 	pactl set-card-profile alsa_card.pci-0000_01_00.1 off
 	pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
-	sleep 1
+	sleep 10
 done
