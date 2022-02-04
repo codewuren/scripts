47741c2a30f3c834a9812dbff92430c1f8faadba
codewuren <codewuren@gmail.com>
2022-02-04 23:22:11 +0800

    remove the useless loop

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,5 @@
 #/bin/bash
 
-COUNT=$(ps -ef |grep bspwm |grep -v "grep" |wc -l)
-
 xinput --set-prop "pointer:Logitech G304" "libinput Accel Speed" -0
 xrandr --dpi 144
 xset s off
@@ -9,11 +7,6 @@ xset dpms 0 0 0
 xset r rate 200 35
 sleep 2
 pgrep -x sxhkd > /dev/null || sxhkd -m 1&
-
-while [ $COUNT -ne 0 ]
-do
-	pactl set-card-profile alsa_card.pci-0000_01_00.1 off
-	pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
-	setxkbmap us colemak
-	sleep 1
-done
+pactl set-card-profile alsa_card.pci-0000_01_00.1 off
+pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
+setxkbmap us colemak
