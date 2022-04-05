04ee04e3180f25ce8f503513300859b276a6a27a
codewuren <codewuren@gmail.com>
2022-04-05 17:45:07 +0800

    add dwm scripts

--- autostart.sh
+++ autostart.sh
@@ -2,11 +2,14 @@
 
 xinput --set-prop "pointer:Logitech G304" "libinput Accel Speed" -0
 xrandr --dpi 144
+dunst -conf ~/.config/dunst/dunstrc &
+picom &
+nm-applet &
+fcitx5 &
+feh --bg-fill --randomize ~/Pictures/Wallpapers/* &
 xset s off
 xset dpms 0 0 0
 xset r rate 200 35
-sleep 2
-pgrep -x sxhkd > /dev/null || sxhkd -m 1&
+sh ~/scripts/dwm-status-bar-refresh.sh &
 pactl set-card-profile alsa_card.pci-0000_01_00.1 off
 pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
-setxkbmap us colemak
---
+++ dwm-status-bar-refresh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while true; do
+	/bin/bash ~/scripts/dwm-status-bar.sh
+	sleep 1
+done
