4f73846af16b29065776eaa3179105c54d23ca54
CodeWuRen <codewuren@gmail.com>
2023-11-16 19:07:22 +0800

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,6 @@
 #/bin/bash
 
 xinput --set-prop 12 'libinput Accel Profile Enabled' 0, 1
-xrandr --dpi 144
 xinput --set-prop 12 'libinput Accel Speed' -0
 dunst -conf ~/.config/dunst/dunstrc &
 picom &
@@ -11,6 +10,5 @@ feh --bg-fill --randomize ~/Pictures/Wallpapers/* &
 xset s off
 xset dpms 0 0 0
 xset r rate 200 35
-/bin/bash ~/scripts/dwm-status-bar-refresh.sh &
 pactl set-card-profile alsa_card.pci-0000_01_00.1 off
 pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
